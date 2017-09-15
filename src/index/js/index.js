46eb1909dd3dc9e57032f415f988e8bdc2cff04e
Doug <douglas.pinheiro@icloud.com>
2017-09-15 11:44:29 -0300

    Alterações no alertEmail

--- src/index/js/index.js
+++ src/index/js/index.js
@@ -2,7 +2,6 @@
 $(document).ready(function(){
 	$('#form-email').on('submit',function(event) {
 		event.preventDefault();
-		/* Act on the event */
 
 		$.ajax({
 		   url: 'index/ajax/enviaemail.php',
@@ -14,18 +13,16 @@ $(document).ready(function(){
 				console.log(result);
 				if (result.success=='0'){
 					console.log('erro ao enviar email');
-					$('.alert-erro').show();
-
+					alertaEmailErro();
 
 				} else if (result.success=='1'){
 					console.log('sucesso!');
-					$('.alert-emailRecebido').show();
+					alertaEmailRecebido();
 				} 
 		})
 		.fail(function() {
-		   console.log("error");
-			$('.alert-erro').show();
-		   
+		   	console.log("error");
+			alertaEmailErro();
 
 		})
 		.always(function() {
@@ -35,3 +32,20 @@ $(document).ready(function(){
 	});
 
 });
+
+function alertaEmail(classe,textoTitulo,textoLongo){
+	$('.textoTitulo').text(textoTitulo);
+	$('.textoLongo').text(textoLongo);
+	$('.alertaEmail')
+	.removeClass('alert-warning')
+	.removeClass('alert-success')
+	.addClass(classe).show();
+}
+
+function alertaEmailRecebido(){
+	alertaEmail('alert-success','Feito!',' Recebemos seus dados, entraremos em contato o quanto antes.')
+}
+
+function alertaEmailErro(){
+	alertaEmail('alert-warning','Desculpe.',' Nosso servidor está em manutenção, por gentileza, nos envie um email para: faleconosco@informaticos.net.br')
+}

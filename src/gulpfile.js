ecf032cf8e836abdf12cae3d5d6ec8192b346ae0
Doug <douglas.pinheiro@icloud.com>
2017-09-15 07:31:26 -0300

    Otimizando os diretorios

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -29,28 +29,35 @@ gulp.task("server",function(){
 	    proxy: "127.0.0.1/sistemaeventos/src/",
 	    ghostMode: false
 	});
-	gulp.watch("src/index.html").on("change",browserSync.reload);
-	gulp.watch("src/index/css/*.css").on("change",browserSync.reload);
-	gulp.watch("src/index/js/*.js").on("change",browserSync.reload);
+	gulp.watch("index.html").on("change",browserSync.reload);
+	gulp.watch("index/css/*.css").on("change",browserSync.reload);
+	gulp.watch("index/js/*.js").on("change",browserSync.reload);
 });
 
 //Limpa a pasta dist
 gulp.task("clean",function(){
-	return gulp.src("dist/").pipe(clean());
+	gulp.src("../index/js/").pipe(clean({force: true}));
+	gulp.src("../index/css/").pipe(clean({force: true}));
+	gulp.src("../index/imagens/").pipe(clean({force: true}));
+	gulp.src("../index/ajax/").pipe(clean({force: true}));
+
+
+
+	return gulp.src("../index.html").pipe(clean({force: true}));
 });
 
 //copia os arquivos selecionados
 gulp.task("copy",['clean'],function(){
 
-	gulp.src("src/index/imagens/**/*").pipe(gulp.dest("dist/index/imagens/"));
-	gulp.src("src/index/ajax/**/*").pipe(gulp.dest("dist/index/ajax/"));
+	gulp.src("index/imagens/**/*").pipe(gulp.dest("../index/imagens/"));
+	gulp.src("index/ajax/**/*").pipe(gulp.dest("../index/ajax/"));
 	return;
 });
 
 //Builda o index, com o css e js.
 gulp.task("build",function(){
 
-	gulp.src('src/index.html')
+	gulp.src('index.html')
 	.pipe(usemin({
 	css: [rev],
 	html: [ function () {return htmlmin({ collapseWhitespace: true, removeComments: true});} ],
@@ -60,6 +67,6 @@ gulp.task("build",function(){
 	inlinejs: [ function (){ return babili({mangle: { keepClassNames: true}})} ],
 	inlinecss: [ cleanCss, 'concat' ]
 	}))
-	.pipe(gulp.dest('dist/'));
+	.pipe(gulp.dest('../'));
 });
 

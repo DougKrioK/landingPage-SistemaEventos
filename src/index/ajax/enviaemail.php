346d76cd2f5a3fa4e7ee8e38109dd652dc430d00
Doug <douglas.pinheiro@icloud.com>
2017-10-04 15:56:18 -0300

    Incluindo GA

--- src/index/ajax/enviaemail.php
+++ src/index/ajax/enviaemail.php
@@ -8,89 +8,85 @@ $emailContato = $_POST['email'];
 $assunto = $_POST['assunto'];
 $ReceberNoticias = $_POST['ReceberNoticias'];
 
-
 $textoEmail = utf8_decode("
 <html>
 
 	<body>
 		<div align='center'>
 			<table cellspacing='5' cellpadding='5' border='0' width='600' style='background: none repeat scroll 0% 0% rgb(244 , 244 , 244) ; border: 1px solid rgb(102 , 102 , 102)'>
-			    <tbody>
-			        <tr>
-			            <th style='background-color: rgb(204 , 204 , 204)'>Novidades.</th>
-			        </tr>
-			        <tr>
-			            <td valign='top' style='text-align: left'>Oi,<br>
-				            <br>
-				            Alguém preencheu nosso formulário de contato.<br>
-				            <br>
-				            Nome: <strong>{$nome}</strong><br>
-				            Email: <strong>{$emailContato}</strong><br>
-				            Assunto: <strong>{$assunto}</strong><br>
-				            ReceberNoticias: <strong>{$ReceberNoticias}</strong><br>
-				            <hr>
-			            </td>
-			        </tr>
-			        <tr>
-			            <td style='text-align: left'>
-			            	<em>Obrigado,<br>
-			            	Informáticos Sistemas<br>
-			            	</em>
-			            </td>
-			        </tr>
-			    </tbody>
+				<tbody>
+					<tr>
+						<th style='background-color: rgb(204 , 204 , 204)'>Novidades.</th>
+					</tr>
+					<tr>
+						<td valign='top' style='text-align: left'>Oi,<br>
+							<br>
+							Alguém preencheu nosso formulário de contato.<br>
+							<br>
+							Nome: <strong>{$nome}</strong><br>
+							Email: <strong>{$emailContato}</strong><br>
+							Assunto: <strong>{$assunto}</strong><br>
+							ReceberNoticias: <strong>{$ReceberNoticias}</strong><br>
+							<hr>
+						</td>
+					</tr>
+					<tr>
+						<td style='text-align: left'>
+							<em>Obrigado,<br>
+							Informáticos Sistemas<br>
+							</em>
+						</td>
+					</tr>
+				</tbody>
 			</table>
 		</div>
 	</body>
 </html>
 ");
 	
-	use PHPMailer\PHPMailer\PHPMailer;
-	use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-	require '../../vendor/autoload.php';
+require '../../vendor/autoload.php';
 
-	$mail = new PHPMailer(true);
-	try {
-	    //Server settings
-	    //$mail->SMTPDebug = 2;                                 // Enable verbose debug output
-	    $mail->isSMTP();                                      // Set mailer to use SMTP
-	    $mail->Host = $hostServidor;  // Specify main and backup SMTP servers
-	    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-	    $mail->Username = $emailCliente;                 // SMTP username
-	    $mail->Password = $senhaEmailCliente;                           // SMTP password
-	    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-	    $mail->Port = 587;                                    // TCP port to connect to
+$mail = new PHPMailer(true);
+try {//tento envira o email, se der certo, retorno 1
 
-	    //Recipients
-	    $mail->setFrom($emailEmpresa, 'Mailer');
-	    $mail->addAddress($douglasEmail);     // Add a recipient
-	    $mail->addAddress($gustavoEmail);               // Name is optional
+	//Server settings
+	//$mail->SMTPDebug = 2;// Enable verbose debug output
+	$mail->isSMTP();// Set mailer to use SMTP
+	$mail->Host = $hostServidor;  // Specify main and backup SMTP servers
+	$mail->SMTPAuth = true;// Enable SMTP authentication
+	$mail->Username = $emailCliente;// SMTP username
+	$mail->Password = $senhaEmailCliente;// SMTP password
+	$mail->SMTPSecure = 'tls';// Enable TLS encryption, `ssl` also accepted
+	$mail->Port = 587;// TCP port to connect to
 
-	    //Attachments
-	    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-	    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+	//Recipients
+	$mail->setFrom($emailEmpresa, 'Mailer');
+	$mail->addAddress($douglasEmail);// Add a recipient
+	$mail->addAddress($gustavoEmail);// Name is optional
 
-	    //Content
-	    //$mail->isHTML(true);                                  // Set email format to HTML
-	    $mail->Subject = 'Dados de contato na landing page';
-	    //$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-	    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-	    $mail->msgHTML($textoEmail);
+	//Attachments
+	//$mail->addAttachment('/var/tmp/file.tar.gz');// Add attachments
+	//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');// Optional name
 
-	    $mail->send();
-	    	$resultado = array(
-	        	"success" => '1',
-	        );
+	//Content
+	//$mail->isHTML(true);// Set email format to HTML
+	$mail->Subject = 'Dados de contato na landing page';
+	//$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+	//$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+	$mail->msgHTML($textoEmail);
 
-	} catch (Exception $e) {
-    	$resultado = array(
-        	"success" => '0',
-        	'Mailer Error' => $mail->ErrorInfo
-        );
-	}
+	$mail->send();
+	$resultado = array(
+		"success" => '1',
+	);
 
-
-
-
+} catch (Exception $e) { //se der erro no envio do email, retorno 0, e o erro junto
+	$resultado = array(
+		"success" => '0',
+		'Mailer Error' => $mail->ErrorInfo
+	);
+}
 echo json_encode($resultado); 
